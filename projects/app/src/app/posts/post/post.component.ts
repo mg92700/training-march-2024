0f87cca482b0c48aa632fc797979c4e0615b4ee5
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-03-26 10:45:45 +0100

    Control flow

--- projects/app/src/app/posts/post/post.component.ts
+++ projects/app/src/app/posts/post/post.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../models/post';
-import { NgIf } from '@angular/common';
+
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.css'],
     standalone: true,
-    imports: [NgIf]
+    imports: []
 })
 export class PostComponent  {
   @Input({required: true})
--- projects/app/src/app/posts/posts/posts.component.ts
+++ projects/app/src/app/posts/posts/posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { PostsService } from '../posts.service';
-import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
@@ -8,7 +8,7 @@ import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
     templateUrl: './posts.component.html',
     styleUrls: ['./posts.component.css'],
     standalone: true,
-    imports: [RouterOutlet, NgIf, NgFor, RouterLinkActive, RouterLink, AsyncPipe]
+    imports: [RouterOutlet, RouterLinkActive, RouterLink, AsyncPipe]
 })
 export class PostsComponent {
   protected posts$ = inject(PostsService).getAll();
