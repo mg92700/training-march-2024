a7b8539172fe11fe0c87b5aeec934c2ccf9dc129
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-03-27 09:09:11 +0100

    Formulaire tp 3

--- projects/my-lib/src/lib/auto-filter-drop-down/auto-filter-drop-down.component.ts
+++ projects/my-lib/src/lib/auto-filter-drop-down/auto-filter-drop-down.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { BoldSearchTextDirective } from '../directives/bold-search-text.directive';
 import { ClickOutsideDirective } from '../directives/click-outside.directive';
 import { FilteredListPipe } from '../pipes/filtered-list.pipe';
@@ -11,15 +11,24 @@ import { FilteredListPipe } from '../pipes/filtered-list.pipe';
     FormsModule, FilteredListPipe, BoldSearchTextDirective, ClickOutsideDirective
   ],
   templateUrl: './auto-filter-drop-down.component.html',
-  styleUrl: './auto-filter-drop-down.component.css'
+  styleUrl: './auto-filter-drop-down.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: AutoFilterDropDownComponent,
+      multi: true
+    }
+  ]
 })
-export class AutoFilterDropDownComponent {
+export class AutoFilterDropDownComponent implements ControlValueAccessor {
   protected searchValue?: string;
   protected openned = false;
 
   set value(value: string) {
     this.searchValue = value;
     this.openned = false;
+    this.onChange(value);
+    this.onTouched();
   }
 
   @Input()
@@ -29,5 +38,21 @@ export class AutoFilterDropDownComponent {
 
   onOptionClick(option: string) {
     this.value = option;
+  }
+
+  private onChange = (value: string) => {};
+  private onTouched = () => {};
+
+  writeValue(obj?: string): void {
+    if (obj)
+      this.searchValue = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 }
